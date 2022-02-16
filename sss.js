a01152b834a8fe42dd3babb697c5448c992be927
Ivka Dimin <rass219@mail.ru>
2022-02-16 13:05:38 +0700

    java3

--- sss.js
+++ sss.js
@@ -4,9 +4,22 @@ function generate() {
 	for (let i = 0; i < n; i++) {
 		arr[i] = Math.floor(Math.random()*10)
 	}
+	print()
+}
+
+function zerofill() {
+let n = parseInt(document.getElementById('a').value)
+for(let i = 0; i < n; i++) {
+	arr[i] = 0
+	}
+	print()
+}
+function print(){
+let n = parseInt(document.getElementById('a').value)
 let div = document.getElementById('mes')
+div.innerHTML = ''
 for(let i = 0; i < n; i++) {
 	div.innerHTML += arr[i] + ' '
 }
-}
 
+}
