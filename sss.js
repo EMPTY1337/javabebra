04b77587c32b2b6f3b298e2a0398c38f30b440d8
Ivka Dimin <rass219@mail.ru>
2022-03-02 11:13:45 +0700

    java3

--- sss.js
+++ sss.js
@@ -1,34 +1,23 @@
-let students = [
-{'id': 1,'name': 'Дима', 'surname': 'а' },
-{'id': 2,'name': 'Олег', 'surname': 'п' },
-{'id': 3,'name': 'Данил', 'surname': 'р' },
-{'id': 4,'name': 'Платина', 'surname': 'Ф' }
-
-]
+let students = []
 let current = 0
 
+
+
+function loadFromSite(){
+	$.get('http://217.71.129.139:4035/students.php', function(data){
+		students = JSON.parse(data)['response']
+	})
+}
 function load_student(id){
-	document.getElementById('name').value = students[id].name
-	document.getElementById('surname').value = students[id].surname
+	$("#name").val(students[id].name)
+	$("#surname").val(students[id].surname)
 }
 
 function next(){
-if (current < students.length){
-	load_student(current)
+	load_student(current) 
 	current++
-	console.log(current)
-}
+	
 if  (current === students.length){
-	document.getElementById('btn_next').disabled = true
-}
-}
-function previous(){
-if (current < students.length){
-	current--
-	load_student(current)
-	console.log(current)
-}
-if  (current === 0){
-	document.getElementById('btn_prev').disabled = true
+	//('btn_next').disabled = true
 }
 }
