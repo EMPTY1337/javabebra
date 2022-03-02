f9e8da3eeebc23b63565d621f4841008264a538b
Ivka Dimin <rass219@mail.ru>
2022-03-02 13:20:32 +0700

    java4

--- sss.js
+++ sss.js
@@ -1,23 +1,37 @@
 let students = []
-let current = 0
+
 
 
 
 function loadFromSite(){
 	$.get('http://217.71.129.139:4035/students.php', function(data){
 		students = JSON.parse(data)['response']
-	})
-}
-function load_student(id){
-	$("#name").val(students[id].name)
-	$("#surname").val(students[id].surname)
+		show_table()
+	});
 }
 
-function next(){
-	load_student(current) 
-	current++
+function show_table(){
+let tbody = $('#tbl_all>tbody')
+tbody.html('')
+for ( let i = 0; i < students.length; i++) {
 	
-if  (current === students.length){
-	//('btn_next').disabled = true
+	let tr = $('<tr></tr>')
+	let td1 = $('<td>' + students[i].id + '</td>')
+	
+	let td2 = $('<td>' + students[i].surname + '</td>')
+	let td3 = $('<td>' + students[i].name + '</td>')
+	let td4 = $(`<td><button onclick ="show_info(${i})">Подробнее</button></td>`)
+	tr.append(td1).append(td3).append(td2).append(td4)
+	tbody.append(tr)
+
+		}
+
 }
+function show_info(id){
+	let div = $('#info')
+	let header = $('<h1>Информация о студенте №'+(id+1)+'</h1>')
+	let name = $('<h3>'+students[id].name+' '+students[id].surname+'</h3>')
+	let img = $(`<img src="http://217.71.129.139:4035/${students[id].logo}>`)
+	div.html('')
+	div.append(header).append(name).append(img)
 }
