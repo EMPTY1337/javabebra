d2f3d2ff75829b6350735dc8619b96cafd475ae2
Ivka Dimin <rass219@mail.ru>
2022-03-09 11:02:46 +0700

    canva4

--- canvas/draw.js
+++ canvas/draw.js
@@ -1,11 +1,10 @@
 function draw(){
-	
-	 let canvas = document.getElementById('canva');
+	let canvas = document.getElementById('canva');
 	 let ctx = canvas.getContext('2d');
-	  ctx.beginPath();
-	  ctx.moveTo(250,120);
-      ctx.lineTo(50,100);
-      ctx.lineTo(150,50);
-      ctx.lineTo(250,120);
-       ctx.fill();
+	ctx.lineWidth = 5; 
+	ctx.arc(200,200,75,0, 2*Math.PI,true);
+	ctx.strokeStyle = "orange";
+	ctx.stroke();
+	 ctx.fillStyle = "pink"; //цвет заливки
+   ctx.fill(); 
 	}
