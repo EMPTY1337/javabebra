f05885b60dfdbec1f5390bf052c010cd1f41e5a5
Ivka Dimin <rass219@mail.ru>
2022-03-09 11:14:50 +0700

    canva5

--- canvas/draw.js
+++ canvas/draw.js
@@ -1,10 +1,11 @@
 function draw(){
 	let canvas = document.getElementById('canva');
 	 let ctx = canvas.getContext('2d');
-	ctx.lineWidth = 5; 
-	ctx.arc(200,200,75,0, 2*Math.PI,true);
-	ctx.strokeStyle = "orange";
-	ctx.stroke();
-	 ctx.fillStyle = "pink"; //цвет заливки
-   ctx.fill(); 
+
+   ctx.moveTo(200,100);
+// команда рисования линии с координатами конца линии
+ctx.lineTo(500,200);
+ctx.strokeStyle = "pink"; //цвет линии
+ctx.lineWidth = "10"; //толщина линии
+ctx.stroke(); // обводка линии
 	}
